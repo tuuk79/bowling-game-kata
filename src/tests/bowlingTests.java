4dbc1d21c987f9536d0354808d0dca4d28e84771
tuuk79 <stevecyoo@gmail.com>
2017-02-26 15:34:45 -0600

    adding tests

--- src/tests/bowlingTests.java
+++ src/tests/bowlingTests.java
@@ -7,8 +7,36 @@ import org.junit.Test;
 public class bowlingTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void bowlingCompetitionShouldHaveAtLeastOnePlayer() {
+		Competition competition = new Competition();
+		Player player = new Player();
+		
+		competition.players.add(player);
+
+		assertTrue(competition.getPlayers() > 0);
+	}
+	
+	@Test
+	public void eachPlayerCanPlayGames() {
+		Player player = new Player();
+		player.games = new ArrayList<Game>();
+		
+		Game game = new Game();
+		player.games.add(game);
+		
+		assertTrue(player.games > 0);
+	}
+	
+
+	
+	@Test
+	public void playerRollsAndGameReturnsNumberOfPinsKnockedDown() {
+		Game game = new Game();
+		game.currentPlayer = game.getPlayer();
+		int pinsKnockedDown = game.currentPlayer.roll();
+		game.currentPlayer.totalPinsKnockedDown = pinsKnockedDown;
+
+		assertEquals(game.currentPlayer.totalPinsKnockedDown, pinsKnockedDown);
 	}
 
 }
