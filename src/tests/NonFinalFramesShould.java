90ffd853dff0889174a2a903e6ab91e82671e914
Sean <sjgallen@gmail.com>
2017-03-09 22:15:56 -0600

    passed one test

--- src/tests/NonFinalFramesShould.java
+++ src/tests/NonFinalFramesShould.java
@@ -11,7 +11,7 @@ import main.NonFinalFrame;
 
 public class NonFinalFramesShould {
 	private Frame frame;
-	
+	private NonFinalFrame nonFinalFrame;
 	@Before
 	public void setup() {
 		 nonFinalFrame = new NonFinalFrame();
