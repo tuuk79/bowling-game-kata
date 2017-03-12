823cb865862e9968e7cde8b719aef0854426546e
Sean <sjgallen@gmail.com>
2017-03-11 21:45:15 -0600

    refactored classes

--- src/main/Frame.java
+++ src/main/Frame.java
@@ -3,7 +3,7 @@ package main;
 import java.util.ArrayList;
 
 public abstract class Frame {
-	public ArrayList<Attempt> attempts;
+	public ArrayList<Attempt> attempts = new ArrayList<Attempt>();
 	private Boolean isComplete = false;
 	
 	public void markAsComplete() {
@@ -12,5 +12,16 @@ public abstract class Frame {
 
 	public boolean getCompletionStatus() {
 		return isComplete;
+	}
+	
+	public ArrayList<Attempt> getAttempts() {
+		return attempts;
+	}
+	
+	public void setMaxAttempt(int maxAttempts) {
+		
+		for (int i = 0; i < maxAttempts; i++) {
+			attempts.add(new Attempt());
+		}
 	}
 }
--- src/main/NonFinalFrame.java
+++ src/main/NonFinalFrame.java
@@ -1,18 +1,13 @@
 package main;
 
-import java.util.ArrayList;
 
 public class NonFinalFrame extends Frame {
 	public int maxAttempts = 2;
 	
 	public NonFinalFrame() {
-		attempts = new ArrayList<Attempt>();
-		for (int i = 0; i < maxAttempts; i++) {
-			attempts.add(new Attempt());
-		}
+		
+		setMaxAttempt(maxAttempts);
 	}
 
-	public ArrayList<Attempt> getAttempts() {
-		return attempts;
-	}
+	
 }
