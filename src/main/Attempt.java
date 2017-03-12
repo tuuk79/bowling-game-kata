fb56e5c0fe1b8d46ca67252ab25212801fab698f
Sean <sjgallen@gmail.com>
2017-03-11 21:06:58 -0600

    removed a needless constructor method

--- src/main/Attempt.java
+++ src/main/Attempt.java
@@ -3,10 +3,6 @@ package main;
 public class Attempt {
 	public int totalPinsKnockedDown = 0;
 
-	public Attempt() {
-		
-	}
-	
 	public int getPinsKnockedDown() {
 		return totalPinsKnockedDown;
 	}
